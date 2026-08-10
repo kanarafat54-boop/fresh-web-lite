6f6c6a81225556d1beae37254f9e54e9ded7a86d
kanarafat54-boop <kanarafat54@gmail.com>
2026-08-10 22:00:52 +0330

    refactor(layout): remove obsolete context barrel

--- src/app/contexts/LayoutContext.tsx
+++
@@ -1,4 +0,0 @@
-export { LayoutProvider } from "./LayoutProvider";
-export { useLayout } from "./useLayout";
-export { LayoutContext } from "./LayoutContextValue";
-export type { LayoutState, LayoutActions } from "./LayoutTypes";
