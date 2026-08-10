fa6e8fc91d15e2709edcff8d99d4f5eab186208a
kanarafat54-boop <kanarafat54@gmail.com>
2026-08-10 13:07:22 +0330

    feat(ai): expose unified intelligence fabric

---
+++ src/features/ai/intelligence/index.ts
@@ -0,0 +1,7 @@
+import "./registerConnectors";
+
+export * from "./aiKnowledge";
+export * from "./intelligenceConnectors";
+export * from "./intelligenceRouter";
+export * from "./wisdomKnowledge";
+export { webResearchConnector } from "./webResearchConnector";
