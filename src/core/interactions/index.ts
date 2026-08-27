2d2e227f398f437ca84427ef7ce58b9e3ec82103
kanarafat54-boop <kanarafat54@gmail.com>
2026-08-27 03:20:05 -0700

    Wire universal interaction service and persistence through canonical barrel

--- src/core/interactions/index.ts
+++ src/core/interactions/index.ts
@@ -1,4 +1,6 @@
 export * from "./FreshReactionModel";
+export * from "./FreshInteractionService";
+export * from "./FreshInteractionPersistence";
 export * from "./FreshInteractionA11y";
 export * from "./FreshAttachmentValidation";
 export * from "./FreshInteractionComponents";
